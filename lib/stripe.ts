cb2cc4662dd042efecd8725268d78b6c5dce015e
YachtieBot <yachtiebot@miamiyachting.com>
2026-02-17 22:13:25 +0000

    Use correct Stripe API version 2026-01-28.clover

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder', {
-  apiVersion: '2024-10-28.acacia',
+  apiVersion: '2026-01-28.clover',
   typescript: true,
 });
