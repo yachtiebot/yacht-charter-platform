67e036423df7ae083737e2028ae22580f4a89b00
YachtieBot <yachtiebot@miamiyachting.com>
2026-02-26 15:34:16 +0000

    Change 'accompaniments' to 'extras' for mobile fit
    MOBILE FIX:
    - 'accompaniments' was wrapping to 2 lines
    - Now '2 extras' (shorter, fits on one line)
    - Cleaner, more casual-chic
    - Perfect mobile layout
    
    Deploying now...

--- app/test-customization/page.tsx
+++ app/test-customization/page.tsx
@@ -51,7 +51,7 @@ export default function TestCustomizationPage() {
             <svg className="w-4 h-4 text-[#c4a265] mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
-            <span style={{ fontWeight: 300 }}>2 accompaniments</span>
+            <span style={{ fontWeight: 300 }}>2 extras</span>
           </li>
         </ul>
         
